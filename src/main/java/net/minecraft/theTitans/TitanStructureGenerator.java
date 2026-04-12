0ce8fd76959f4b132aab8178cf2fdb098388ec35
mx-wj <2471811489@qq.com>
2026-04-13 01:21:42 +0800

    Optimize WorldGenOmegafish

--- src/main/java/net/minecraft/theTitans/TitanStructureGenerator.java
+++ src/main/java/net/minecraft/theTitans/TitanStructureGenerator.java
@@ -1,18 +1,13 @@
 /*
  * Decompiled with CFR 0.152.
- * 
- * Could not load the following classes:
- *  cpw.mods.fml.common.IWorldGenerator
- *  net.minecraft.init.Blocks
- *  net.minecraft.world.World
- *  net.minecraft.world.chunk.IChunkProvider
- *  net.minecraft.world.gen.feature.WorldGenerator
  */
 package net.minecraft.theTitans;
 
 import cpw.mods.fml.common.IWorldGenerator;
 import java.util.Random;
 import net.minecraft.init.Blocks;
+import net.minecraft.theTitans.perf.PerfSection;
+import net.minecraft.theTitans.perf.TitansPerf;
 import net.minecraft.theTitans.world.WorldGenBlazeTitan;
 import net.minecraft.theTitans.world.WorldGenCaveSpiderTitan;
 import net.minecraft.theTitans.world.WorldGenCreeperTitan;
@@ -29,55 +24,47 @@ import net.minecraft.theTitans.world.WorldGenZombieTitan;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenerator;
-import net.minecraft.theTitans.perf.PerfSection;
-import net.minecraft.theTitans.perf.TitansPerf;
 
-public class TitanStructureGenerator
-implements IWorldGenerator {
+public class TitanStructureGenerator implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
         long perfNs = TitansPerf.begin();
         try {
-        int dimensionId = world.provider.dimensionId;
-        if (dimensionId == -1) {
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenMagmaCubeTitan(Blocks.netherrack), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenGhastTitan(Blocks.netherrack), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenWitherSkeletonTitan(Blocks.netherrack), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenBlazeTitan(Blocks.netherrack), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenPigZombieTitan(Blocks.netherrack), 256);
-        } else if (dimensionId != 1 && dimensionId != -1 && dimensionId != 200 && dimensionId != 201) {
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenOmegafish(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenCaveSpiderTitan(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSpiderTitan(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSpiderJockeyTitan(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenZombieTitan(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSkeletonTitan(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenCreeperTitan(Blocks.stone), 256);
-            this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSlimeTitan(Blocks.stone), 256);
-        }
-    
-        }
-        finally {
+            int dimensionId = world.provider.dimensionId;
+            if (dimensionId == -1) {
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenMagmaCubeTitan(Blocks.netherrack), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenGhastTitan(Blocks.netherrack), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenWitherSkeletonTitan(Blocks.netherrack), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenBlazeTitan(Blocks.netherrack), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenPigZombieTitan(Blocks.netherrack), 256);
+            } else if (dimensionId != 1 && dimensionId != -1 && dimensionId != 200 && dimensionId != 201) {
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenOmegafish(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenCaveSpiderTitan(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSpiderTitan(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSpiderJockeyTitan(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenZombieTitan(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSkeletonTitan(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenCreeperTitan(Blocks.stone), 256);
+                this.generateStructure(random, world, chunkX * 16, chunkZ * 16, new WorldGenSlimeTitan(Blocks.stone), 256);
+            }
+        } finally {
             TitansPerf.endWarn(PerfSection.STRUCTURE_GEN, "TitanStructureGenerator#dim" + world.provider.dimensionId, perfNs);
         }
-}
+    }
 
     private void generateStructure(Random rand, World world, int chunkX, int chunkZ, WorldGenerator worldgen, int maxY) {
         long perfNs = TitansPerf.begin();
         try {
-        int x = chunkX * 16 + rand.nextInt(16);
-        int z = chunkZ * 16 + rand.nextInt(16);
-        int randPosY = rand.nextInt(world.getActualHeight());
-        if (world.provider.dimensionId != -1) {
-            randPosY = world.getTopSolidOrLiquidBlock(x, z);
-        }
-        if (rand.nextInt(2) == 0) {
-            worldgen.generate(world, rand, x, randPosY, z);
-        }
-    
-        }
-        finally {
+            int x = chunkX + rand.nextInt(16);
+            int z = chunkZ + rand.nextInt(16);
+            int randPosY = rand.nextInt(world.getActualHeight());
+            if (world.provider.dimensionId != -1) {
+                randPosY = world.getTopSolidOrLiquidBlock(x, z);
+            }
+            if (rand.nextInt(2) == 0) {
+                worldgen.generate(world, rand, x, randPosY, z);
+            }
+        } finally {
             TitansPerf.endWarn(PerfSection.WORLD_GEN, worldgen.getClass().getSimpleName(), perfNs);
         }
+    }
 }
-}
-
--- src/main/java/net/minecraft/theTitans/world/WorldGenOmegafish.java
+++ src/main/java/net/minecraft/theTitans/world/WorldGenOmegafish.java
@@ -1,52 +1,66 @@
 /*
  * Decompiled with CFR 0.152.
- * 
- * Could not load the following classes:
- *  net.minecraft.block.Block
- *  net.minecraft.entity.Entity
- *  net.minecraft.world.World
- *  net.minecraft.world.gen.feature.WorldGenerator
- *  org.apache.logging.log4j.LogManager
  */
 package net.minecraft.theTitans.world;
 
+import java.util.List;
 import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.titan.EntitySilverfishTitan;
 import net.minecraft.entity.titan.EntityTitan;
 import net.minecraft.theTitans.TheTitans;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import org.apache.logging.log4j.LogManager;
 
-public class WorldGenOmegafish
-extends WorldGenerator {
-    private Block field_150520_a;
+public class WorldGenOmegafish extends WorldGenerator {
+    private final Block field_150520_a;
 
     public WorldGenOmegafish(Block p_i45464_1_) {
         this.field_150520_a = p_i45464_1_;
     }
 
     public boolean generate(World worldIn, Random rng, int x, int y, int z) {
+        if (worldIn == null || worldIn.isRemote) {
+            return false;
+        }
+        if (rng.nextInt(20) != 0) {
+            return false;
+        }
+        if (y <= 1 || y >= worldIn.getActualHeight() - 2) {
+            return false;
+        }
+
         if (!worldIn.getBlock(x, y - 1, z).isOpaqueCube()) {
             --y;
         } else if (worldIn.getBlock(x, y, z).isOpaqueCube()) {
             ++y;
-        } else if (!worldIn.isRemote && worldIn.isAirBlock(x, y, z) && worldIn.getBlock(x, y - 1, z).isOpaqueCube() && rng.nextInt(20) == 0) {
-            EntitySilverfishTitan entityomegafish = new EntitySilverfishTitan(worldIn);
-            entityomegafish.destroyBlocksInAABBGriefingBypass(entityomegafish.boundingBox);
-            entityomegafish.onSpawnWithEgg(null);
-            entityomegafish.setLocationAndAngles((float)x + 0.5f, y, (float)z + 0.5f, rng.nextFloat() * 360.0f, 0.0f);
-            EntityTitan otherTitan = (EntityTitan)worldIn.findNearestEntityWithinAABB(EntityTitan.class, entityomegafish.boundingBox.expand(200.0, 200.0, 200.0), (Entity)entityomegafish);
-            if (otherTitan != null) {
-                return false;
-            }
-            worldIn.spawnEntityInWorld((Entity)entityomegafish);
-            LogManager.getLogger(TheTitans.class).info("Found a succesfully spawned Omegafish at " + x + ", " + y + ", " + z + ", spawning.");
-            return true;
         }
-        return false;
+
+        if (y <= 1 || y >= worldIn.getActualHeight() - 2) {
+            return false;
+        }
+        if (!worldIn.blockExists(x, y, z) || !worldIn.blockExists(x, y - 1, z)) {
+            return false;
+        }
+        if (!worldIn.isAirBlock(x, y, z) || !worldIn.getBlock(x, y - 1, z).isOpaqueCube()) {
+            return false;
+        }
+
+        AxisAlignedBB nearbyBox = AxisAlignedBB.getBoundingBox((double)x - 96.0, (double)y - 48.0, (double)z - 96.0, (double)x + 96.0, (double)y + 48.0, (double)z + 96.0);
+        List nearbyTitans = worldIn.getEntitiesWithinAABB(EntityTitan.class, nearbyBox);
+        if (nearbyTitans != null && !nearbyTitans.isEmpty()) {
+            return false;
+        }
+
+        EntitySilverfishTitan entityomegafish = new EntitySilverfishTitan(worldIn);
+        entityomegafish.setLocationAndAngles((float)x + 0.5f, y, (float)z + 0.5f, rng.nextFloat() * 360.0f, 0.0f);
+        entityomegafish.onSpawnWithEgg(null);
+
+        worldIn.spawnEntityInWorld((Entity)entityomegafish);
+        LogManager.getLogger(TheTitans.class).info("Found a succesfully spawned Omegafish at " + x + ", " + y + ", " + z + ", spawning.");
+        return true;
     }
 }
-
